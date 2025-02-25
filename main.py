646f7d8ff58180fdbe5258bca6a68b9e8224f39e
= <swooplida@gmail.com>
2025-02-25 21:34:26 +0500

    Перевод с английского на русский и наоборот

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import os
+from flask import Flask
 from deep_translator import GoogleTranslator
 from lingua import Language, LanguageDetectorBuilder
 import config
 import telebot
-from langdetect import detect
+import threading
 
 bot = telebot.TeleBot(config.token)
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Бот работает!"
+
 languages = [Language.ENGLISH, Language.RUSSIAN]
 detector = LanguageDetectorBuilder.from_languages(*languages).build()
 
@@ -15,4 +23,7 @@ def trans(message):
     else:
         translated_text = GoogleTranslator(source='ru', target='en').translate(message.text)
     bot.send_message(message.chat.id,translated_text)
+def run():
+    app.run(host='0.0.0.0', port=8080)
+threading.Thread(target=run).start()
 bot.polling()
